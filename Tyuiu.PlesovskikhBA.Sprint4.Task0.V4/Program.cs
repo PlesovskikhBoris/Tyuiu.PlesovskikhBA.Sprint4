242504247a5936f0de09850ff7c80a22f50eb468
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-06 18:17:07 +0500

    Add project files.

---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task0.V4/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint4.Task0.V4.Lib;
+
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task0.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9. Подсчитать произведение  *");
+            Console.WriteLine("* чётных элементов массива:  { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 }             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsArray = { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+
+            Console.WriteLine("Исходный массив: ");
+
+            for (int i = 0; i < numsArray.Length; i++)
+            {
+                Console.WriteLine(numsArray[i]);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Произведение чётных элементов массива = {ds.GetMultEvenArrEl(numsArray)}");
+            Console.ReadKey();
+        }
+    }
+}
