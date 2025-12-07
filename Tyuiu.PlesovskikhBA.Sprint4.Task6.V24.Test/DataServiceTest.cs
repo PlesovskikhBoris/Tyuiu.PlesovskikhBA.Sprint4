115fe45c9d31433e7372f8992367c0cee35b6a25
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-07 20:06:20 +0500

    Task6

---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task6.V24.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint4.Task6.V24.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task6.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Checked()
+        {
+            DataService ds = new DataService();
+            string[] array = ["Компьютер", "Телефон", "Планшет", "Принтер", "Сканер", "Монитор", "Клавиатура"];
+            string[] wait = ["Компьютер", "Клавиатура"];
+            string[] res = ds.Calculate(array);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task6.V24/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint4.Task6.V24.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task6.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных [`Компьютер`, `Телефон`, `Планшет`,         *");
+            Console.WriteLine("* `Принтер`,`Сканер`, `Монитор`, `Клавиатура`], используя класс Array,    *");
+            Console.WriteLine("* выведите элементы массива, длина которых больше 7 символов.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив: ");
+            string[] array = ["Компьютер", "Телефон", "Планшет", "Принтер", "Сканер", "Монитор", "Клавиатура"];
+            foreach (string i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string[] res = ds.Calculate(array);
+            Console.WriteLine("Элементы массива, длина которых больше 7 символов: ");
+            foreach (string i in res)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
