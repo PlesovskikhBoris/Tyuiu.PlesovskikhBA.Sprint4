9184692f8f48545684f06d37a556fc749cd7be8e
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-07 19:37:17 +0500

    Task4

---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task4.V26/Program.cs
@@ -0,0 +1,70 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint4.Task4.V26.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task4.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#4                                                                *");
+            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнила: Плесовских Борис Александрович | СМАРТб-24-1                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 5. Заменить нечетные        *");
+            Console.WriteLine("* элементы массива на 0.                                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите кол-во строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите кол-во столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[rows, columns];
+
+            Console.WriteLine("Введите элементы массива");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+
+            }
+
+            Console.WriteLine("Ваш массив");
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int[,] res = ds.Calculate(array);
+
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    Console.Write(res[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
