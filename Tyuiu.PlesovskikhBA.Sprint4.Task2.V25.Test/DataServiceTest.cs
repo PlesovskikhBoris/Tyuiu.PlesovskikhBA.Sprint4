d46e1d14f35d9676481991bb6013a007a86d1816
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-07 18:05:25 +0500

    Task2

---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task2.V25.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint4.Task2.V25.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task2.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 8, 3, 5, 4, 4, 5, 3, 7, 6, 9, 5 };
+            Assert.AreEqual(768, ds.Calculate(array));
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task2.V25/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Tyuiu.PlesovskikhBA.Sprint4.Task2.V25.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task2.V25
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Спринт #4 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 11 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 3 до 9 подсчитать произведение четных         *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Random rn = new Random();
+            int[] ar = new int[11];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = rn.Next(3, 9);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.Write("Массив: ");
+            foreach (int x in ar)
+                Console.Write(x + " ");
+            Console.Write("\n\r");
+            DataService ds = new DataService();
+            int result = ds.Calculate(ar);
+            Console.WriteLine($"Произведение четных элементов массива = {result}");
+        }
+    }
+}
