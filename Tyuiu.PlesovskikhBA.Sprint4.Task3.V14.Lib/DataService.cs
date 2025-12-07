0306a79dbac1dc62ee7dcf9e9f8742f53d11e293
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-07 19:19:36 +0500

    Task3

---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task3.V14.Lib
+{
+    public class DataService : ISprint4Task3V14
+    {
+        public int Calculate(int[,] array)
+        {
+            int maxInFourthRow = array[3, 0];
+
+            for (int j = 1; j < array.GetLength(1); j++)
+            {
+                if (array[3, j] > maxInFourthRow)
+                {
+                    maxInFourthRow = array[3, j];
+                }
+            }
+            return maxInFourthRow;
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task3.V14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Tyuiu.PlesovskikhBA.Sprint4.Task3.V14.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task3.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Спринт #4 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int[,] array = {
+            {3, 2, 3, 3, 5},
+            {2, 3, 3, 7, 3},
+            {7, 5, 2, 7, 3},
+            {4, 2, 7, 5, 2},
+            {3, 5, 4, 2, 6}
+        };
+            var result = ds.Calculate(array);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
