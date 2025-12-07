ad750f48e42335fdb34859b5630ce7fa4ebbfc75
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-07 20:55:29 +0500

    Task7

---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint4.Task7.V13.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task7.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            int rows = 3;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "159357246";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint4.Task7.V13/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using Tyuiu.PlesovskikhBA.Sprint4.Task7.V13.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint4.Task7.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int rows = 3;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "159357246";
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы                                                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* подсчитайте количество четных чисел                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int index = 0;
+
+            Console.WriteLine("\n Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(rows, columns, str);
+            Console.WriteLine("Количество четных чисел = " + res);
+            Console.ReadKey();
+        }
+    }
+}
